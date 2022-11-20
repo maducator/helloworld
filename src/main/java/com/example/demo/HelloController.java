41c66f0f24ca4c12241ada71ed8c880d2aaae1e8
madstorm <madstorm.lee@gmail.com>
2022-11-20 19:26:13 +0900

    Controller에서 GET을 받도록 함.

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
 
 	@GetMapping
-	String hello() {
-		return "Hello World!";
+	String hello(@RequestParam(defaultValue="world") String hello) {
+		return "Hello " + hello + "!";
 	}
 }
